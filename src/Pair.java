72054a1ff10168593f51bcb4b8457298f6104b9f
ahmedlabib02 <ahmedlabibamer@gmail.com>
2023-03-19 01:51:47 +0200

    ...

---
+++ src/Pair.java
@@ -0,0 +1,25 @@
+public class Pair {
+    private Object min;
+    private Object max;
+
+    public Pair(Object min, Object max){
+        this.min=min;
+        this.max=max;
+    }
+
+    public Object getMin() {
+        return min;
+    }
+
+    public void setMin(Object min) {
+        this.min = min;
+    }
+
+    public Object getMax() {
+        return max;
+    }
+
+    public void setMax(Object max) {
+        this.max = max;
+    }
+}
